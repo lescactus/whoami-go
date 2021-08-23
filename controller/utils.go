ce459b901c3a8ff900e1fdc5172a157bbf9176f9
Alexandre Maldémé <alma@moodagent.com>
2021-08-23 12:22:14 +0200

    Refactoring: Optimize removeCustomHeaders() by using regexp.MustCompile

--- controller/utils.go
+++ controller/utils.go
@@ -2,14 +2,15 @@ package controller
 
 import "regexp"
 
-const (
-	// Regex to remove all 'x-' or 'X-' HTTP headers (Ex: 'X-Forwarded-For')
-	headersToRemoveRegex = "(^((forwarded|Forwarded).*)|((x|X)-(.*))$)"
+var (
+	// Regex to remove all 'x-' or 'X-' HTTP headers (Ex: 'X-Forwarded-For' or 'X-Forwarded-Proto')
+	headersToRemoveRegex = regexp.MustCompile("(^((forwarded|Forwarded).*)|((x|X)-(.*))$)") 
 )
 
 func removeCustomHeaders(h map[string]string) map[string]string {
+
 	for header := range h {
-		matched, _ := regexp.MatchString(headersToRemoveRegex, header)
+		matched := headersToRemoveRegex.MatchString(header)
 		if matched {
 			delete(h, header)
 		}
